266ba544cbead0a2933e547542e01a8dc3f7b835
Juan Ignacio Macias Perez <juanmaciasperez89@gmail.com>
2024-03-30 00:34:33 +0000

    hotfix img source

--- js/script.js
+++ js/script.js
@@ -11,40 +11,40 @@ const text4 = document.querySelector(".question4 p");
 
 buttonShowText1.addEventListener("click", function () {
   if (buttonShowText1.src.endsWith("icon-plus.svg")) {
-    buttonShowText1.src = "../assets/images/icon-minus.svg";
+    buttonShowText1.src = "./assets/images/icon-minus.svg";
     text1.classList.remove("invisible");
   } else {
-    buttonShowText1.src = "../assets/images/icon-plus.svg";
+    buttonShowText1.src = "./assets/images/icon-plus.svg";
     text1.classList.add("invisible");
   }
 });
 
 buttonShowText2.addEventListener("click", function () {
   if (buttonShowText2.src.endsWith("icon-plus.svg")) {
-    buttonShowText2.src = "../assets/images/icon-minus.svg";
+    buttonShowText2.src = "./assets/images/icon-minus.svg";
     text2.classList.remove("invisible");
   } else {
-    buttonShowText2.src = "../assets/images/icon-plus.svg";
+    buttonShowText2.src = "./assets/images/icon-plus.svg";
     text2.classList.add("invisible");
   }
 });
 
 buttonShowText3.addEventListener("click", function () {
   if (buttonShowText3.src.endsWith("icon-plus.svg")) {
-    buttonShowText3.src = "../assets/images/icon-minus.svg";
+    buttonShowText3.src = "./assets/images/icon-minus.svg";
     text3.classList.remove("invisible");
   } else {
-    buttonShowText3.src = "../assets/images/icon-plus.svg";
+    buttonShowText3.src = "./assets/images/icon-plus.svg";
     text3.classList.add("invisible");
   }
 });
 
 buttonShowText4.addEventListener("click", function () {
   if (buttonShowText4.src.endsWith("icon-plus.svg")) {
-    buttonShowText4.src = "../assets/images/icon-minus.svg";
+    buttonShowText4.src = "./assets/images/icon-minus.svg";
     text4.classList.remove("invisible");
   } else {
-    buttonShowText4.src = "../assets/images/icon-plus.svg";
+    buttonShowText4.src = "./assets/images/icon-plus.svg";
     text4.classList.add("invisible");
   }
 });
