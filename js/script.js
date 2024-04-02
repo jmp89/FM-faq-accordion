cdbd084f8f5184ddd514bff5668cd5ca4e421e8e
Juan Ignacio Macias Perez <juanmaciasperez89@gmail.com>
2024-04-02 17:27:47 +0100

    mediaquery background fix

--- js/script.js
+++ js/script.js
@@ -1,50 +1,31 @@
 "use strict";
 
-const buttonShowText1 = document.querySelector(".button1 img");
-const buttonShowText2 = document.querySelector(".button2 img");
-const buttonShowText3 = document.querySelector(".button3 img");
-const buttonShowText4 = document.querySelector(".button4 img");
-const text1 = document.querySelector(".question1 p");
-const text2 = document.querySelector(".question2 p");
-const text3 = document.querySelector(".question3 p");
-const text4 = document.querySelector(".question4 p");
-
-buttonShowText1.addEventListener("click", function () {
-  if (buttonShowText1.src.endsWith("icon-plus.svg")) {
-    buttonShowText1.src = "./assets/images/icon-minus.svg";
-    text1.classList.remove("invisible");
+function toggleVisibility(button, text) {
+  if (button.src.endsWith("icon-plus.svg")) {
+    button.src = "./assets/images/icon-minus.svg";
+    text.classList.remove("invisible");
   } else {
-    buttonShowText1.src = "./assets/images/icon-plus.svg";
-    text1.classList.add("invisible");
+    button.src = "./assets/images/icon-plus.svg";
+    text.classList.add("invisible");
   }
-});
+}
 
-buttonShowText2.addEventListener("click", function () {
-  if (buttonShowText2.src.endsWith("icon-plus.svg")) {
-    buttonShowText2.src = "./assets/images/icon-minus.svg";
-    text2.classList.remove("invisible");
-  } else {
-    buttonShowText2.src = "./assets/images/icon-plus.svg";
-    text2.classList.add("invisible");
-  }
-});
+const buttons = [
+  document.querySelector(".button1 img"),
+  document.querySelector(".button2 img"),
+  document.querySelector(".button3 img"),
+  document.querySelector(".button4 img"),
+];
 
-buttonShowText3.addEventListener("click", function () {
-  if (buttonShowText3.src.endsWith("icon-plus.svg")) {
-    buttonShowText3.src = "./assets/images/icon-minus.svg";
-    text3.classList.remove("invisible");
-  } else {
-    buttonShowText3.src = "./assets/images/icon-plus.svg";
-    text3.classList.add("invisible");
-  }
-});
+const texts = [
+  document.querySelector(".question1 p"),
+  document.querySelector(".question2 p"),
+  document.querySelector(".question3 p"),
+  document.querySelector(".question4 p"),
+];
 
-buttonShowText4.addEventListener("click", function () {
-  if (buttonShowText4.src.endsWith("icon-plus.svg")) {
-    buttonShowText4.src = "./assets/images/icon-minus.svg";
-    text4.classList.remove("invisible");
-  } else {
-    buttonShowText4.src = "./assets/images/icon-plus.svg";
-    text4.classList.add("invisible");
-  }
+buttons.forEach((button, index) => {
+  button.addEventListener("click", function () {
+    toggleVisibility(button, texts[index]);
+  });
 });
